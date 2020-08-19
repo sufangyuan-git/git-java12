f490128b20a1486f2aadf08891fc048be8341a04
sufangyuan-gits <1551721627@qq.com>
2020-08-19 18:21:26 +0800

    2

--- eleven/src/eleven/hello.java
+++ eleven/src/eleven/hello.java
@@ -6,6 +6,8 @@ public class hello {
 		// TODO Auto-generated method stub
 		System.out.println("hello world!");
 		System.out.println("hello world!-01");
+		System.out.println("hello world!-02");
+		
 	}
 
 }
