040292a11221f9b2130bc856ccaf4f8c9bd857b1
贤狼赫萝IT园 <69194088+sufangyuan-git@users.noreply.github.com>
2020-08-19 16:14:56 +0800

    First.java

--- first/src/first/First.java
+++ first/src/first/First.java
@@ -16,6 +16,7 @@ public class First {
 		System.out.println("hello  world!");
 		System.out.println("hi!");
 		System.out.println("say hi!");
+		System.out.println("say");
 		/*在first\src\first中输入 javac -encoding utf-8 First.java
 		 * cd ..回到src文件下
 		 **执行 Java 包名.类名(java first.First)*/
